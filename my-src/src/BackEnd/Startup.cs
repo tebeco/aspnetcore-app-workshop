1ec11262577bc8c07571621a750d06186c3d0f08
tebeco <tebeco@gmail.com>
2019-06-30 15:16:19 +0200

    adding EF Core / DbContext / Bootstrap / First Model

--- my-src/src/BackEnd/Startup.cs
+++ my-src/src/BackEnd/Startup.cs
@@ -28,6 +28,19 @@ namespace BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                }
+                else
+                {
+                    options.UseSqlite("Data Source=conferences.db");
+                }
+            });
+
             services.AddControllers();
         }
 
